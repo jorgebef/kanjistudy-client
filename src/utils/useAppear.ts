f146b61d42bc98ebdb7c928c9958219853384db0
Jorge Befan <jorge.befan@gmail.com>
2022-01-30 23:30:31 +0100

    updating the useAppear hook for appear on scroll components

--- src/utils/useAppear.ts
+++ src/utils/useAppear.ts
@@ -1,39 +1,60 @@
-import { useCallback, useEffect, useState } from 'react'
+import { RefObject, useCallback, useEffect, useState } from 'react'
 
-const useAppear = (ref: any) => {
+const useAppear = (ref: RefObject<Element>) => {
   const [isVisible, setIsVisible] = useState(false)
 
   const callback = useCallback(
-    entries => {
+    (entries: IntersectionObserverEntry[]) => {
+      // To use hook, the children that want to be toggled need to have class 'aos'
+      // ---------------------------------------------------------------------------
+      // Extract the toggle function which is called on intersect and leaving intersect
+      const AOSappear = (c: HTMLCollection) => {
+        Array.from(c).forEach((i: Element) => {
+          if (i.classList.contains('aos')) {
+            i.classList.add('appear')
+          }
+        })
+      }
+      const AOSdisappear = (c: HTMLCollection) => {
+        Array.from(c).forEach((i: Element) => {
+          if (i.classList.contains('aos')) {
+            i.classList.remove('appear')
+          }
+        })
+      }
+      // run the checks whether the ref intersects or leaves intersect
       const [entry] = entries
       if (entry.isIntersecting) {
-        ref?.current?.classList.add('appear')
-        console.log('VISIBLE')
+        // ref?.current?.firstElementChild?.classList.add('appear')
+        const children = ref?.current?.children
+        if (children) AOSappear(children)
         setIsVisible(true)
       } else {
-        ref?.current?.classList.remove('appear')
-        console.log('NOT VISIBLE')
+        // ref?.current?.firstElementChild?.classList.remove('appear')
+        const children = ref?.current?.children
+        if (children) AOSdisappear(children)
         setIsVisible(false)
       }
     },
     [ref]
   )
 
-  const options = {
+  const options: IntersectionObserverInit = {
     root: null,
     rootMargin: '0px',
     threshold: 1.0,
   }
 
   useEffect(() => {
-    const observer = new IntersectionObserver(callback, options)
-    const currRef = ref?.current
+    const observer: IntersectionObserver = new IntersectionObserver(
+      callback,
+      options
+    )
+    const currRef: Element | null = ref?.current
     if (currRef) observer.observe(currRef)
 
-    return () => {
-      if (currRef) observer.unobserve(currRef)
-    }
-  }, [ref, callback, options])
+    return () => observer.disconnect()
+  }, [ref, callback])
 
   return isVisible
 }

45a389d2dfd89e324748b9f3339259cb0f380b4b
Jorge Befan <jorge.befan@gmail.com>
2022-02-07 23:34:09 +0100

    adding functionality to darken only background of button

--- src/components/common/Button.styles.ts
+++ src/components/common/Button.styles.ts
@@ -1,17 +1,21 @@
 import styled from 'styled-components'
 
-export const BtnRed = styled.button`
-  border: none;
-  cursor: pointer;
-  font-size: 1.3rem;
-  font-weight: 500;
-  color: ${p => p.theme.white};
-  background-color: ${p => p.theme.red};
-  border-radius: ${p => p.theme.buttonR};
-  align-self: center;
-  padding: 0.5rem 1.5rem;
-  transition: all 0.2s ease-in-out;
-`
+// Taken from stackoverflow, shameless copy paste (but understanding what's happening)
+// https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+// THANK YOU supersan
+function adjustColor(color: string, amount: number) {
+  return (
+    '#' +
+    color
+      .replace(/^#/, '')
+      .replace(/../g, color =>
+        (
+          '0' +
+          Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)
+        ).substr(-2)
+      )
+  )
+}
 
 export const Btn = styled.button`
   text-decoration: none;
@@ -28,7 +32,7 @@ export const Btn = styled.button`
   transition: all ${p => p.theme.transition};
 
   &:hover {
-  background-color: ${p => p.theme.red};
-    filter: brightness(85%);
+    background-color: ${p => adjustColor(p.theme.red, -30)};
+    /* filter: brightness(85%); */
   }
 `
